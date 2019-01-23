ae247a2a0b3ce95eb6ee1ed32d8d483e2ab1c7a8
Lilo Heinrich <liloheinrich@Lilos-MacBook-Pro.local>
2019-01-23 14:16:38 -0800

    writing adjustedencoder to correct encoder vals for pidcontroller

--- src/main/java/org/usfirst/frc/team2557/robot/subsystems/SwerveModule.java
+++ src/main/java/org/usfirst/frc/team2557/robot/subsystems/SwerveModule.java
@@ -7,7 +7,9 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import org.usfirst.frc.team2557.robot.RobotMap;
 import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import org.usfirst.frc.team2557.robot.AdjustedEncoder;
 
 public class SwerveModule extends Subsystem {
 	private final double[] PID;
@@ -17,6 +19,7 @@ public class SwerveModule extends Subsystem {
 	private CANSparkMax speedMotor;
 	private PIDController pidController;
 	private AnalogInput encoder;
+	private AdjustedEncoder adjEnc;
 
 	private double setpoint;
 	public double error;
@@ -25,26 +28,36 @@ public class SwerveModule extends Subsystem {
 	public double rawSetpoint;
 	public double mod;
 	public double yOffset;
+
+	// public int sec;
+	// public Timer timer;
 	
 	public SwerveModule(int swerveModIndex, boolean inverted) {
 		speedMotor = new CANSparkMax(swerveModIndex, MotorType.kBrushless);
 		angleMotor = new WPI_TalonSRX(swerveModIndex);
 		angleMotor.setInverted(inverted);
 		encoder = new AnalogInput(swerveModIndex);
+		adjEnc = new AdjustedEncoder(encoder);
+
 		PID = RobotMap.PIDconst[swerveModIndex];
 		setpointOffset = RobotMap.setpointOffset[swerveModIndex];
 
-		pidController = new PIDController(PID[0], PID[1], PID[2], encoder, angleMotor, RobotMap.pidLoopTime);
+		pidController = new PIDController(PID[0], PID[1], PID[2], adjEnc, angleMotor, RobotMap.pidLoopTime);
 		pidController.setInputRange(0.0, RobotMap.circumference);
 		pidController.setOutputRange(-1.0, 1.0);
 		pidController.setContinuous(true);
-		// pidController.setAbsoluteTolerance(RobotMap.toleranceAnglePID);
+		pidController.setAbsoluteTolerance(RobotMap.toleranceAnglePID);
 		pidController.enable();
 
 		angleMotor.configContinuousCurrentLimit(30, 0);
 		angleMotor.configPeakCurrentLimit(30, 0);
 		angleMotor.configPeakCurrentDuration(100, 0);
 		angleMotor.enableCurrentLimit(true);
+
+		// timer = new Timer();
+		// timer.reset();
+		// timer.start();
+		// sec = 2;
 	}
 
 	public double getEncoderCount(){
@@ -61,6 +74,9 @@ public class SwerveModule extends Subsystem {
 		setpoint = (angle + 1.0) * RobotMap.circumference / 2.0;
 		setpoint = setpoint + setpointOffset;
 		setpoint %= RobotMap.circumference;
+		// System.out.print("setpoint: " + setpoint);
+
+		adjEnc.setSetpoint(setpoint);
 		pidController.setSetpoint(setpoint);
 
 		// double halfCir = RobotMap.circumference/2;
@@ -75,9 +91,23 @@ public class SwerveModule extends Subsystem {
 		// if (Math.abs(output) < RobotMap.toleranceAnglePID) { output = 0.0; }
 		// angleMotor.set(output); // set motor
 
-		error = pidController.getError();
-		output = pidController.get();
-		encPID = encoder.pidGet(); // -2.048 to 2.048
+		// error = pidController.getError();
+		// System.out.print(" error: " + error);
+		// output = pidController.get();
+		// System.out.print(" output: " + output);
+		// encPID = encoder.pidGet(); // -2.048 to 2.048
+		// System.out.print(" encPID: " + encPID);
+		// System.out.println(" time: " + timer.get());
+
+		// if(timer.get() > sec){
+		// 	sec += 2;
+		// 	if(sec/2 % 2 == 0){
+		// 		pidController.setSetpoint(0.0);
+		// 	}else{
+		// 		pidController.setSetpoint(2.0);
+		// 	}
+		// }
+		// timer.reset();
 	}
 
 
