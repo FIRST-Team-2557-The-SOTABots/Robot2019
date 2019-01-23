56f8e2edabda1c9b5a5660cb6fab665861fa6f2e
Lilo Heinrich <liloheinrich@Lilos-MacBook-Pro.local>
2019-01-23 11:41:39 -0800

    changes from yesterday

--- src/main/java/org/usfirst/frc/team2557/robot/subsystems/SwerveModule.java
+++ src/main/java/org/usfirst/frc/team2557/robot/subsystems/SwerveModule.java
@@ -34,9 +34,9 @@ public class SwerveModule extends Subsystem {
 		PID = RobotMap.PIDconst[swerveModIndex];
 		setpointOffset = RobotMap.setpointOffset[swerveModIndex];
 
-		pidController = new PIDController(PID[0], PID[1], PID[2], encoder, angleMotor);
-		pidController.setInputRange(0, RobotMap.circumference);
-		pidController.setOutputRange(-1, 1);
+		pidController = new PIDController(PID[0], PID[1], PID[2], encoder, angleMotor, RobotMap.pidLoopTime);
+		pidController.setInputRange(0.0, RobotMap.circumference);
+		pidController.setOutputRange(-1.0, 1.0);
 		pidController.setContinuous(true);
 		// pidController.setAbsoluteTolerance(RobotMap.toleranceAnglePID);
 		pidController.enable();
@@ -45,11 +45,6 @@ public class SwerveModule extends Subsystem {
 		angleMotor.configPeakCurrentLimit(30, 0);
 		angleMotor.configPeakCurrentDuration(100, 0);
 		angleMotor.enableCurrentLimit(true);
-
-		// System.out.println("is acc ch: " + encoder.isAccumulatorChannel());
-		// encoder.initAccumulator();
-		// encoder.setAccumulatorInitialValue(0);
-		// encoder.resetAccumulator();
 	}
 
 	public double getEncoderCount(){
@@ -63,9 +58,10 @@ public class SwerveModule extends Subsystem {
 	public void drive (double speed, double angle) { // angle = -1 to 1
 		speedMotor.set (speed);
 
-		setpoint = ((angle + 1.0) * RobotMap.circumference * 1000.0 / 2.0);
-		setpoint = (setpoint + setpointOffset * 1000.0) % (RobotMap.circumference * 1000.0);
-		pidController.setSetpoint(setpoint / 1000.0);
+		setpoint = (angle + 1.0) * RobotMap.circumference / 2.0;
+		setpoint = setpoint + setpointOffset;
+		setpoint %= RobotMap.circumference;
+		pidController.setSetpoint(setpoint);
 
 		// double halfCir = RobotMap.circumference/2;
 		// setpoint = angle * halfCir; // -2.048 to 2.048
@@ -74,7 +70,6 @@ public class SwerveModule extends Subsystem {
 		// mod = setpoint % halfCir;
 		// yOffset = - Math.floor(setpoint/halfCir) * halfCir;
 		// setpoint = setpoint % halfCir - Math.floor(setpoint/halfCir) * halfCir; // still -2.048 to 2.048
-
 		// error = setpoint - encPID; // + or - diff 
 		// output = PID[0] * error / halfCir; // kP * error / cir = motorPower
 		// if (Math.abs(output) < RobotMap.toleranceAnglePID) { output = 0.0; }
@@ -87,8 +82,7 @@ public class SwerveModule extends Subsystem {
 
 
     public void initDefaultCommand() {
-		// NOTE: there should be no default command here 
-		// unless you plan to run only one swerve module at a time
-
+		// NOTE: should be no default command here unless
+		// planning to run swerve modules independantly
     }
 }
