75601ca3a5b11ffa225799fbedaa14b6c362b260
Lilo Heinrich <liloheinrich@M-lilo.local>
2019-04-02 21:47:25 -0700

    idk

--- src/main/java/org/usfirst/frc/team2557/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team2557/robot/Robot.java
@@ -15,10 +15,6 @@ import org.usfirst.frc.team2557.robot.subsystems.Climber;
 import org.usfirst.frc.team2557.robot.subsystems.GyroSwerveDrive;
 import org.usfirst.frc.team2557.robot.subsystems.Intake;
 import org.usfirst.frc.team2557.robot.subsystems.Lift;
-
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.command.Command;
@@ -44,7 +40,6 @@ public class Robot extends TimedRobot {
 	PIDlift mb;
 	PIDlift my;
 	PIDlift mx;
-	VisionDriveStraightOn vdso;
 	VisionWithGyro vwg;
 	ClimbCommandGroup c3;
 	RetractClimb rc;
@@ -81,7 +76,6 @@ public class Robot extends TimedRobot {
 		mb = new PIDlift(RobotMap.midPos);
 		my = new PIDlift(RobotMap.highPos);
 		mx = new PIDlift(RobotMap.xPos);
-		vdso = new VisionDriveStraightOn();
 		vwg = new VisionWithGyro();
 		c3 = new ClimbCommandGroup(16500);
 		rc = new RetractClimb();
